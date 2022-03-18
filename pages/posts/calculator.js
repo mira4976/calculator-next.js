6f8bdbac2be5d638809af33a716cf9fb2470f80a
mira4976 <95692671+mira4976@users.noreply.github.com>
2022-03-18 20:43:38 +0900

    nem create

--- pages/posts/calculator.js
+++ pages/posts/calculator.js
@@ -1,46 +1,42 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../../componets/layout'
-import Script from 'next/script'
-
 
     
 export default function Home() {
   
   return (
     <Layout>
+      
     <Head>
       
       <title>calculator</title>
     </Head>
  
-    <body>
-  <div class="calculator-box">
-  
-    <div class="display"><textarea name="" id="display" cols="30"></textarea></div>
-    <button  id = "btn">0</button> 
-    <button >1</button> 
-    <button>2</button> 
-    <button >3</button> 
-    <button >4</button> 
-    <button >5</button> 
-    <button >6</button> 
-    <button >7</button>
-    <button >8</button>
-    <button >9</button>
-    <button >.</button>
-    <button >=</button>
-    <button >+</button>
-    <button >-</button>
-    <button >❌</button>
-    <button >÷</button>
-    <button >AC</button>
+    <div><textarea id = "display" cols="30" defaultValue={0}></textarea></div>
+
+    <button className='addnumber' value={0} onClick={addnumber(0)}>0</button> 
+    <button className='addnumber' value={1}>1</button> 
+    <button className='addnumber' value={2}>2</button> 
+    <button className='addnumber' value={3}>3</button> 
+    <button className='addnumber' value={4}>4</button> 
+    <button className='addnumber' value={5}>5</button> 
+    <button className='addnumber' value={6}>6</button> 
+    <button className='addnumber' value={7}>7</button>
+    <button className='addnumber' value={8}>8</button>
+    <button className='addnumber' value={9}>9</button>
+    <button className='addnumber' value={'.'}>.</button>
+    <button className='addcalculator'>=</button>
+    <button className='addcalculator'>+</button>
+    <button className='addcalculator'>-</button>
+    <button className='addcalculator'>❌</button>
+    <button className='addcalculator'>÷</button>
+    <button id='allclear'>AC</button>
     <Link href="/">
     <button>戻る</button>
     </Link>
-  </div>
+  
   <script src="/js/main.js"></script>
-</body>   
     </Layout>
   )
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,104 @@
-alert('JavaScriptのアラート');
+/* let total = 0; //合計の値
+let input = 0; //入力の値
+let symnbol = "+"; //押された演算子
+let numberIn = true //数字がディスプレイに入っているか？
+let numberPush = false; //数字のボタンが押されたか？
+ */
 
-var btn = document.getElementById('btn');
+let total = 0; //合計の値
+let input = 0; //入力の値
+let symnbol = "+"; //押された演算子
+let numberIn = true //数字がディスプレイに入っているか？
+let numberPush = false; //数字のボタンが押されたか？
 
-btn.addEventListener('click', function() {
+/* //数字のボタンが押された時の処理
+function addnumber(number) {
+    if (numberIn == true) { //数字がディスプレイに入っている場合
+      document.querySelector('textarea').value = ""; //ディスプレイの表示を消す
+      numberIn = false; //数字はディスプレイに入っていない
+    }
+    document.querySelector('textarea').value += number; //ディスプレイに数字を表示させていく
+    input = document.getElementById("display").value; //ディスプレイの表示から値を受けとりinputに入れる
+    numberPush = true; //数字のボタンが押された
+    ;
+  }
+ */
 
-    console.log('うんち');
 
-},false);
+//数字のボタンを押した
+var addnumber = document.getElementsByClassName("addnumber");//classNameからドキュメント要素を習得してaddnumberに入れる
+
+for(let i = 0; i < addnumber.length; i++){//forで要素数分ループ処理
+
+    addnumber[i].addEventListener('click',function addnumber(number){
+        if (numberIn == true) { //数字がディスプレイに入っている場合
+            document.querySelector('textarea').value = ""; //ディスプレイの表示を消す
+            numberIn = false; //数字はディスプレイに入っていない
+          }
+          document.querySelector('textarea').value += number; //ディスプレイに数字を表示させていく
+          input = document.getElementById("display").value; //ディスプレイの表示から値を受けとりinputに入れる
+          numberPush = true; //数字のボタンが押された
+          ;
+    },false);
+
+}
+
+/* 
+
+
+
+//演算子のボタンが押された時の処理
+function addoperator(operator) {
+  if (numberPush == true) {  //数字のボタンが押されている場合
+    switch(symnbol){
+      case '+': //+の場合
+        input = parseFloat(input); //文字列を整数に変換
+        total  = total + input; //計算を行う
+        numberIn = true; //数字が入っている
+        document.querySelector('textarea').value = Math.round(total *10)/10; //計算結果をディスプレイに表示する
+        symnbol = operator; //+をsymbolに代入する
+        break; //終了
+      case '-':
+        input = parseFloat(input);
+        total = total - input;
+        numberIn = true;
+        document.querySelector('textarea').value = Math.round(total *10)/10;
+        symnbol = operator;
+        break;
+      case '*':
+        input = parseFloat(input);
+        total = total * input;
+        numberIn = true;
+        document.querySelector('textarea').value = Math.round(total *10)/10;
+        symnbol = operator;
+        break;
+      case '/':
+        input = parseFloat(input);
+        total = total / input;
+        numberIn = true;
+        document.querySelector('textarea').value = Math.round(total *10)/10;
+        symnbol = operator;
+        break;
+      case '=':
+        document.querySelector('textarea').value = Math.round(total *10)/10;
+        numberIn = true;
+        symnbol = "+";
+    }    
+  }
+} */
+
+//ACボタンを押す
+var allclear = document.getElementById('allclear'); //idにあうドキュメント要素を取得してallclearに入れる
+allclear.addEventListener('click',function allclear(){//クリックされたのでallclearを実行する
+    document.querySelector('textarea').value = 1; //ディスプレイの表示をOに初期化する
+    input = 0; //現在入力している値を消す
+    total = 0; //合計の値を初期化する
+    numberIn = true; //数字がディスプレイに入っている
+});
+
+/* function allclear() {
+  document.querySelector('textarea').value = 0; //ディスプレイの表示を消す
+  input = 0; //現在入力している値を消す
+  total = 0; //合計の値を初期化する
+  numberIn = true; //数字がディスプレイに入っている
+} */
